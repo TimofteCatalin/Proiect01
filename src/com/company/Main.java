a8ef3ff23da4b809a4f6f31ea0810746b820b952
Catalin <catalin.timofte@ymail.com>
2016-05-26 17:02:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,67 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        double m1= 20;
+//        double m2= 80;
+//        double m3=m1+m2;
+//        m3*=25;
+//
+//        System.out.println(m3);
+//
+//        m3 %=3;
+//        System.out.println(m3);
+//        if (m3<=20)
+//        System.out.println(m3 + "  Afiseaza" );
+
+//        int var1= 50;
+//
+//        int var2= 50;
+//        var2 /=2;
+//        var1/=2;
+//        var1++;
+//        var2++;
+//        var1--;
+//        var2++;
+//        int tot=var1+var2;
+//        System.out.println(tot);
+//        if (tot>49)
+//            System.out.println(tot);
+//        tot=10;
+//        tot*=2;
+//        if (tot>var1 || tot<var2)
+//            System.out.println("Merge Tot cu valoarea de " +tot);
+//        if (tot<var1 &&  var2>tot)
+//            System.out.println("Merge si aceasta varianta de " + tot);
+//
+//        int da1 = 50;
+//        int da2 = 131;
+//        if (da2 > da1) {
+//            System.out.println(da2 + " nu merge");
+//        } else {
+//            System.out.println(da2 + "da merge");
+//        }
+
+        int d1 = 50;
+        int d2 = 10;
+        d1= d1/1;
+        d1 *= 2;
+        d2= d1;
+        System.out.println(d2);
+        if (d2 <100) {
+            System.out.println(d2);
+        }else if (d2>100){
+            System.out.println("Varianta 2");
+        }else{
+            System.out.println(d2 + " nu merge");
+        }
+
+
+
+
+
+    }
+}
